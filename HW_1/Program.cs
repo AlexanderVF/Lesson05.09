6cac49e8fe70fa87b75e3dcfe441383be1bcbbbb
«alexander <favorite@bk.ru>
2022-09-07 20:11:18 +0300

    HW1

---
+++ HW_1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Сколько чисел Вы хотите ввести?  ");
+int size = int.Parse(Console.ReadLine());
+
+int i = 0;
+string result = "";
+string c = ", ";
+int count = 0;
+
+while (i < size)
+{
+    Console.Write("Введите число: ");
+    int a = int.Parse(Console.ReadLine());
+    result = a + c + result;
+    i++;
+    if (a > 0)
+    {
+        count++;
+    }
+
+}
+
+Console.Write($"Чисел больше -0- среди чисел {result} -> {count} числа");
+
+
---
+++ Les_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int[] GetArray(int size, int minValue, MaxValue) {
+    int[] res = new int[size];
+
+    for (int = 0; int < size; int++) {
+        res[i] = new Random().Next(minValue, maxValue +1);
+    }
+
+    return res;
+}
+
+int[] arr = GetArray(7, 0, 10);
+Console.WriteLine(string.Join(",", arr));
+
+for (int i = 0; i < Array.MaxLength/2; i++) {
+    int temp = arr[i];
+    arr[i] = arr[arr.length - i -1];
+    arr[arr.length - i -1] = temp;
+
+}
+
+Console.WriteLine(string.Join(",", arr));
